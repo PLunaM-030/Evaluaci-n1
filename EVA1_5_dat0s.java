fa08eeaee3312c691dad52bf10f861d3c1806b0b
PLunaM-030 <L25550696@chihuahua2.tecnm.mx>
2025-09-01 23:58:03 -0700

    Add files via upload

---
+++ EVA1_5_dat0s.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_p5_dat0s;
+
+/**
+ *
+ * @author pawlu
+ */
+public class EVA1_P5_dat0s {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar variables
+        //Tipo de dato - Nombre de identificador
+        String nombre;
+        int edad;
+        double salario;
+        //Initiate
+        //Identificador: valor;
+        //= --> operador de asignaci'on
+        
+        nombre= "Paulinn Lunn";
+        edad=18;
+        salario=1.1;
+
+System.out.println(nombre);
+System.out.println(edad);
+System.out.println(salario);
+    }
+    
+}
