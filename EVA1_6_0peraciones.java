f15ee3aff442f30a271d8e87fe24dc8223a67997
PLunaM-030 <L25550696@chihuahua2.tecnm.mx>
2025-09-01 22:04:55 -0700

    Add files via upload

---
+++ EVA1_6_0peraciones.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_0peraciones;
+
+/**
+ *
+ * @author pawlu
+ */
+public class EVA1_6_0peraciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Assign the value of variables
+    double PI;
+    double Area;
+    double Radio;
+    //you can also write all them down in a single line, separated by comma.
+    PI=3.1416;
+    Radio=6.3;
+    Area=PI*(Radio*Radio);
+           
+    System.out.println(Area);
+    }
+    
+}
---
+++ EVA1_7_AreaTriangular.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_areatriangular;
+
+/**
+ *
+ * @author pawlu
+ */
+public class EVA1_7_AreaTriangular {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Area de un triangulo.. operaciones y valores!!
+    double Base;
+    double Altura;
+    double Area;
+    
+    Base=0.5;
+    Altura=0.62;
+    Area=(Base*Altura)/2;
+    
+  System.out.println("El area de un triangulo.. si sus medidas son...");
+  System.out.print("SU BASE ES...");
+  System.out.println(Base);
+  System.out.print("Y ALTURA...");
+  System.out.println(Altura);
+  
+  System.out.print("Entonces su area sale...");
+  System.out.println(Area);
+    
+    }
+    
+}
